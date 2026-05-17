d1aa6065ce005f0c6807d82ff97d499241088ed3
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-05-17 17:25:13 +0530

    Register Capacitor plugins in MainActivity to fix UNIMPLEMENTED native bridge

--- android/app/src/main/java/com/sansoft/songpipe/MainActivity.java
+++ android/app/src/main/java/com/sansoft/songpipe/MainActivity.java
@@ -3,10 +3,16 @@ package com.sansoft.songpipe;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.sansoft.songpipe.extractor.DownloaderImpl;
+import com.sansoft.songpipe.plugins.NativeDownloadPlugin;
+import com.sansoft.songpipe.plugins.NativePlaybackPlugin;
+import com.sansoft.songpipe.plugins.YoutubeExtractorPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(YoutubeExtractorPlugin.class);
+        registerPlugin(NativePlaybackPlugin.class);
+        registerPlugin(NativeDownloadPlugin.class);
         super.onCreate(savedInstanceState);
 
         // Initialize NewPipe Extractor
