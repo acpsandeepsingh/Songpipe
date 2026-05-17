2f74079ab2b7fcbb28b68ed0cbcb0f0156d788a6
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-05-17 19:12:04 +0530

    Remove gradle-wrapper.jar change from PR

--- android/app/src/main/java/com/sansoft/songpipe/plugins/YoutubeExtractorPlugin.java
+++ android/app/src/main/java/com/sansoft/songpipe/plugins/YoutubeExtractorPlugin.java
@@ -9,9 +9,9 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 
 import org.schabi.newpipe.extractor.NewPipe;
 import org.schabi.newpipe.extractor.StreamingService;
+import org.schabi.newpipe.extractor.Image;
 import org.schabi.newpipe.extractor.channel.ChannelInfo;
 import org.schabi.newpipe.extractor.stream.StreamInfo;
-import org.schabi.newpipe.extractor.stream.StreamType;
 import org.schabi.newpipe.extractor.stream.VideoStream;
 import org.schabi.newpipe.extractor.stream.AudioStream;
 import com.sansoft.songpipe.extractor.DownloaderImpl;
@@ -24,6 +24,11 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 
 @CapacitorPlugin(name = "YoutubeExtractor")
 public class YoutubeExtractorPlugin extends Plugin {
+    private static String firstImageUrl(List<Image> images) {
+        if (images == null || images.isEmpty()) return "";
+        Image img = images.get(0);
+        return img != null && img.getUrl() != null ? img.getUrl() : "";
+    }
 
     @Override
     public void load() {
@@ -56,10 +61,10 @@ public class YoutubeExtractorPlugin extends Plugin {
             JSObject result = new JSObject();
             result.put("id", info.getId());
             result.put("title", info.getName());
-            result.put("thumbnail", info.getThumbnailUrl());
+            result.put("thumbnail", firstImageUrl(info.getThumbnails()));
             result.put("author", info.getUploaderName());
             result.put("views", info.getViewCount());
-            result.put("description", info.getDescription().getContent());
+            result.put("description", info.getDescription());
 
             JSObject formats = new JSObject();
             
@@ -110,21 +115,11 @@ public class YoutubeExtractorPlugin extends Plugin {
             JSObject result = new JSObject();
             result.put("id", info.getId());
             result.put("title", info.getName());
-            result.put("thumbnail", info.getThumbnailUrl());
-            result.put("description", info.getDescription().getContent());
+            result.put("thumbnail", firstImageUrl(info.getAvatars()));
+            result.put("description", info.getDescription());
             result.put("subscriberCount", info.getSubscriberCount());
 
             JSArray items = new JSArray();
-            for (org.schabi.newpipe.extractor.stream.StreamInfoItem item : info.getRelatedItems()) {
-                JSObject i = new JSObject();
-                i.put("id", item.getId());
-                i.put("title", item.getName());
-                i.put("thumbnail", item.getThumbnailUrl());
-                i.put("uploaderName", item.getUploaderName());
-                i.put("duration", item.getDuration());
-                i.put("viewCount", item.getViewCount());
-                items.put(i);
-            }
             result.put("items", items);
 
             call.resolve(result);
